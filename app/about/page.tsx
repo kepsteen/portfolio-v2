f48309fdcb988cad2c253801409114f23192a70a
Cody Epstein <codyepstein@gmail.com>
2024-11-07 10:33:24 -0800

    change font size to md

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
 		<article className="mt-8 flex flex-col gap-8 pb-16">
 			<h1 className="font-calistoga text-5xl">about me.</h1>
 			<Socials />
-			<div className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+			<div className="prose max-w-full text-pretty font-sans text-md text-muted-foreground dark:prose-invert">
 				<h2>Bio</h2>
 				<About />
 				<h2>Hobbies</h2>
